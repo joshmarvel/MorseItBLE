b2b08bf686af3314e57c2f63a2b5e5391cc98739
Joshua D. Marvel <joshuam@initiumnovumllc.com>
2020-05-12 19:22:26 +0100

    Added comments and battery voltage monitoring via an LED.

--- MorseItBLE.ino
+++ MorseItBLE.ino
@@ -3,20 +3,30 @@
 //Pin A0 is Dit
 //Pin A1 is Dah
 //All commands sent are the defaults for the Morse-it app
+
 #define MY_DEBUG 0
 #include "BluefruitRoutines.h"
  
-//Pin numbers for switches
+//Pin numbers for paddles
 #define DIT A0
 #define DAH A1
- 
+
+//Battery Monitoring and Indicating
+#define VBAT A9
+#define RED A3
+#define GREEN A4
+#define BLUE A5
+
 //Actions
 #define DO_DIT 1
 #define DO_DAH 2
 #define DO_DITDAH 3
 
-uint8_t i;
+//Track the state of a held switch
+uint8_t held;
 
+
+//Function reads the values of a pin going LOW (Pin to GND)
 uint8_t readSwitches(void) {
   if (digitalRead(DIT) == LOW && digitalRead(DAH) == LOW){return DO_DITDAH;}
   else if (digitalRead(DIT) == LOW){return DO_DIT;}
@@ -24,14 +34,14 @@ uint8_t readSwitches(void) {
   else {return false;}
 }
 
+//Function to send a release key via Bluetooth when the pin goes HIGH (Pin open)
 void Drop(void) {
-  //delay(40);
   ble.println(F("AT+BLEKEYBOARDCODE=00-00"));
   MESSAGE(F("Released"));
   }
 
  
-  //Translate character to keyboard keycode and transmit
+//Main Function, Sends key presses as BLE keyboard characters and monitors battery voltage
 void pressKeyCode (byte c) {
   ble.print(F("AT+BLEKEYBOARDCODE=00-00-"));
   if (c<0x10)ble.print("0");
@@ -40,24 +50,45 @@ void pressKeyCode (byte c) {
   MESSAGE(F("Pressed."));
   delay(50);//de-bounce
   while (readSwitches()) {
-    if (i == 3){
+    if (held == 3){
       if (digitalRead(DIT) == HIGH){Drop();return DO_DAH;}
       else if (digitalRead(DAH) == HIGH){Drop();return DO_DIT;}
     }
-    else if (i == 2){
+    else if (held == 2){
       if (digitalRead(DIT) == LOW && digitalRead(DAH) == LOW){Drop();return DO_DITDAH;}
       else if (digitalRead(DAH) == HIGH){Drop();return DO_DIT;}
     }
-    else if (i == 1){
+    else if (held == 1){
       if (digitalRead(DIT) == LOW && digitalRead(DAH) == LOW){Drop();return DO_DITDAH;}
       else if (digitalRead(DIT) == HIGH){Drop();return DO_DAH;}
     }
   };
   ble.println(F("AT+BLEKEYBOARDCODE=00-00"));
   MESSAGE(F("Released"));
+
+  //Battery Voltage Monitoring, Blue LED for external USB power, Green for Normal power, Red for charge needed.
+  float bvolt = analogRead(VBAT);
+  bvolt *= 2;
+  bvolt *= 3.3;
+  bvolt /= 1024;
+  if (bvolt > 4.2){
+    analogWrite(BLUE, 255);
+    analogWrite(RED, 0);
+    analogWrite(GREEN, 0);
+  }
+  else if (bvolt <= 4.2 && bvolt >= 3.4) {
+    analogWrite(GREEN, 255);
+    analogWrite(BLUE, 0);
+    analogWrite(RED, 0);
+  }
+  else if (bvolt < 3.4){
+    analogWrite(RED, 255);
+    analogWrite(GREEN, 0);
+    analogWrite(BLUE, 0);
+  }
 }
 
-
+//Setup device and assign pinModes
 void setup() {
 #if(MY_DEBUG)
   while (! Serial) {}; delay (500);
@@ -65,14 +96,18 @@ void setup() {
 #endif
   pinMode(DAH, INPUT_PULLUP);
   pinMode(DIT, INPUT_PULLUP);
+  pinMode(GREEN, OUTPUT);
+  pinMode(RED, OUTPUT);
+  pinMode(BLUE, OUTPUT);
   initializeBluefruit();
 }
- 
+
+//Defines the characters to be sent.
 void loop() {
-  i=readSwitches();
-  switch (i) {
-    case DO_DIT: pressKeyCode(0x50);break;
-    case DO_DAH: pressKeyCode(0x4F);break;
-    case DO_DITDAH: pressKeyCode(0x52);break;
+  held=readSwitches();
+  switch (held) {
+    case DO_DIT: pressKeyCode(0x50);break;  //left arrow
+    case DO_DAH: pressKeyCode(0x4F);break;  //right arrow
+    case DO_DITDAH: pressKeyCode(0x52);break;  //up arrow
   }
 }
